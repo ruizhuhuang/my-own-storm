c43a76b2a8ef4206eddc5c48020bd644e5bd70d8
ruizhuhuang <ruizhuhuang@gmail.com>
2017-01-11 16:49:36 -0600

    Initial commit

---
+++ src/main/java/org/example/storm/App.java
@@ -0,0 +1,13 @@
+package org.example.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
